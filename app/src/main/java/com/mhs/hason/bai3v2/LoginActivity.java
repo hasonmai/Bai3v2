2d99c64f9a3b178b6d390b34e7eafb142e09f0cd
hasonmai <hasonmai.sm@gmail.com>
2017-08-16 13:57:33 +0700

    commit changed

---
+++ app/src/main/java/com/mhs/hason/bai3v2/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.mhs.hason.bai3v2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Son Ha on 8/16/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
